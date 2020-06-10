2a0be4f1348e83a03f18e0c78fd2b155a00acd8c
Felipe Lins <contactfelipelins@gmail.com>
2020-06-10 12:24:03 -0400

    set up main app component routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Switch>
+          <Route path='/'></Route>
+          <Route path='/sign-up'></Route>
+          <Route path='/log-in'></Route>
+          <Route path='/home'></Route>
+          <Route path='/:id/details'></Route>
+          <Route path='/:id/artist'></Route>
+          <Route path='/create'></Route>
+          <Route path='/:id/update'></Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
