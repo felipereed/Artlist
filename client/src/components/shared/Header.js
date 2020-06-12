ad30a3745ebc9320c0ae6fee3d7f6d18000a8f53
Felipe Lins <contactfelipelins@gmail.com>
2020-06-12 19:40:07 -0400

    added link to home in header

--- client/src/components/shared/Header.js
+++ client/src/components/shared/Header.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react'
 import './Header.css'
+import { Link } from 'react-router-dom'
 
 export default class Header extends Component {
   render() {
     return (
       <div className='header-image-container'>
+        <Link to='/home'>
         <img className='header-icons' src='images/home.png' alt='home link'></img>
+        </Link>
+        
         <img className='header-icons' src='images/account.png' alt='account link'></img>
       </div>
     )
