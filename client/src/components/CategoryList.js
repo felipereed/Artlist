021e35349efb4fcd72b61a7f55959123add4b075
Felipe Lins <contactfelipelins@gmail.com>
2020-06-11 23:23:45 -0400

    created categories, created horozontal scroll for home component

--- client/src/components/CategoryList.js
+++ client/src/components/CategoryList.js
@@ -1,10 +1,34 @@
 import React, { Component } from 'react'
+import './CategoryList.css'
 
 export default class CategoryList extends Component {
+  categories = [
+    "/images/categories/Group6.png",
+    "/images/categories/Group7.png",
+    "/images/categories/Group8.png",
+    "/images/categories/Group9.png",
+    "/images/categories/Group10.png",
+    "/images/categories/Group11.png",
+    "/images/categories/Group12.png"
+
+  ]
   render() {
     return (
       <div>
-        
+        <div className='categorylist-title'>
+          <h2>Categories</h2>
+        </div>
+        <div className='categorylist-container'>
+          {this.categories.map((category) => 
+            <div className='categorylist-horizontal-scroll' key={category}
+              style={{
+              backgroundImage: `url(${category})`
+            }}
+            >
+              
+            </div>
+          )}
+        </div>
       </div>
     )
   }
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
+import CategoryList from './CategoryList'
+import './Home.css'
 
 export default class Home extends Component {
   render() {
     return (
       <div>
-        
+        <CategoryList />
+        <hr className='home-divider' />
       </div>
     )
   }
