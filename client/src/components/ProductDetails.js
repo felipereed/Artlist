cef5c35934f117c39c9498438b95f72d9ef7c302
Felipe Lins <contactfelipelins@gmail.com>
2020-06-12 20:46:26 -0400

    set up product details api call

--- client/src/components/ProductDetails.js
+++ client/src/components/ProductDetails.js
@@ -1,27 +1,47 @@
-import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { getProduct } from "../services/product";
 
 export default class ProductDetails extends Component {
+  state = {
+    product: null,
+  };
+
+  async componentDidMount() {
+    const product = await getProduct(this.props.productId);
+    this.setState({ product: product });
+  }
+
   render() {
-    const product = {
-      image_url: "https://i.imgur.com/hVwi47j.jpg",
-      name: "Picture 01",
-      price: "$100",
-      username: "felipe",
-      id: "1"
-    }
+    const product = this.state.product;
+
     return (
-      <div className='product-container'>
-        <div className='product-image-container'>
-          <img className='product-image' src={product.image_url} alt='product image'></img>
-        </div>
-        <div className='product-info'>
-        <p>{product.name}</p>
-        <p>{product.price}</p>
-        <p>By {product.username}</p>
-        </div>
-        <Link className='product-more-details-link' to={`/${product.id}/details`} >More details</Link>
-      </div>
-    )
+      <>
+        {this.state.product ? (
+          <div className="product-container">
+            <div className="product-image-container">
+              <img
+                className="product-image"
+                src={product.image_url}
+                alt="product image"
+              ></img>
+            </div>
+            <div className="product-info">
+              <p>{product.name}</p>
+              <p>{product.price}</p>
+              <p>By {product.username}</p>
+            </div>
+            <Link
+              className="product-more-details-link"
+              to={`/${product.id}/details`}
+            >
+              More details
+            </Link>
+          </div>
+        ) : (
+          <div>Loading...</div>
+        )}
+      </>
+    );
   }
 }
--- client/src/services/product.js
+++ client/src/services/product.js
@@ -8,3 +8,8 @@ export const getProducts = async () => {
   const resp = await api.get('/products')
   return resp.data
 }
+
+export const getProduct = async (id) => {
+  const resp = await api.get(`/products/${id}`)
+  return resp.data
+}
