f7b3f0b84bad068ea4e30708440e2c83da466cf7
Felipe Lins <contactfelipelins@gmail.com>
2020-06-13 17:30:26 -0400

    added dropdown menu

--- client/src/components/shared/Header.js
+++ client/src/components/shared/Header.js
@@ -9,10 +9,33 @@ export default class Header extends Component {
         <Link to='/home'>
         <img className='header-icons' src='images/home.png' alt='home link'></img>
         </Link>
-        
         <img className='header-icons' src='images/account.png' alt='account link'></img>
-        <div>
-          
+        <div className='header-dropdown-menu'>
+          <div>
+            <Link className='header-menu-links'>
+              <div className='header-name-and-icons'>
+                <p>Sell your art</p>
+                <img className='header-menu-icons' src='images/sell.png' alt='sell your item'></img>
+              </div>
+            </Link>
+            <hr className="header-dropdown-divider" />
+            <Link className='header-menu-links'>
+              <div className='header-name-and-icons'>
+                <p>View your items</p>
+                <img className='header-menu-icons' src='images/view.png' alt='sell your item'></img>
+              </div>
+            </Link>
+            <hr className="header-dropdown-divider" />
+            <Link className='header-menu-links'>
+              <div className='header-name-and-icons'>
+                <p>Log out</p>
+                <img className='header-menu-icons' src='images/exit.png' alt='sell your item'></img>
+              </div>
+            </Link>
+          </div>
+          <div>
+
+          </div>
         </div>
       </div>
     )
